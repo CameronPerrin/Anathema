dac5c95d54da700cd9d92c98602bddddc7c5ee08
Cameron Perrin <perrin.cameron@gmail.com>
2020-09-06 23:30:27 -0400

    unity Multiplayer tutorial part 4 Completed
    We have lobbies and such

---
+++ Blood And Sand/Assets/Scripts/LobbyMenuScripts/RoomButton.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomButton : MonoBehaviour
+{
+
+	[SerializeField]
+	private Text nameText;
+	[SerializeField]
+	private Text sizeText;
+
+	private string roomName;
+	private int roomSize;
+	private int playerCount;
+
+	public void JoinRoomOnClick()
+	{
+		PhotonNetwork.JoinRoom(roomName);
+	}
+
+	public void SetRoom(string nameInput, int sizeInput, int countInput)
+	{
+		roomName = nameInput;
+		roomSize = sizeInput;
+		playerCount = countInput;
+		nameText.text = nameInput;
+		sizeText.text = countInput + "/" + sizeInput;
+	}
+}
