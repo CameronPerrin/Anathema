b926d2249c946ccce6a0d2f7486ded8815c01ac0
Cameron Perrin <perrin.cameron@gmail.com>
2020-09-09 18:33:54 -0400

    Added a 3rd person cinemachine camera (issue on network)

--- Blood And Sand/Assets/Scripts/PlayerMovementController.cs
+++ Blood And Sand/Assets/Scripts/PlayerMovementController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviourPun
 {
-	public float moveSpeed = 5f;
+    public CharacterController controller;
+    public Transform cam;
+	public float moveSpeed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,13 +26,20 @@ public class PlayerMovementController : MonoBehaviourPun
     		return;
     	}
 
-        if (Input.GetKey("d"))
-            transform.Translate(moveSpeed * Time.deltaTime, 0, 0, Space.World);
-        if (Input.GetKey("a"))
-            transform.Translate(-moveSpeed * Time.deltaTime, 0, 0, Space.World);
-        if (Input.GetKey("w"))
-            transform.Translate(0, 0, moveSpeed * Time.deltaTime, Space.World);
-        if (Input.GetKey("s"))
-            transform.Translate(0, 0, -moveSpeed * Time.deltaTime, Space.World);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+
+
     }
 }
