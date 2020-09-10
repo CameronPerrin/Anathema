0f8e63935c8c75ca4009d5f4e414a5a8db2122c6
trans12 <js_itaki@yahoo.com>
2020-09-09 18:33:58 -0700

    Scene and Building Script added

---
+++ Blood And Sand/Assets/Scripts/PlazaScripts/Buildings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildings : MonoBehaviour
+{
+
+	private String buildingTag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildingTag = gameObject.tag;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void mageTower()
+	{
+		debug.log("You have entered the Mage Tower");
+    }
+}
+}
