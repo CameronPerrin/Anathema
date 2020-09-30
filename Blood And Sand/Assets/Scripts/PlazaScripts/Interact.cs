c67fb55d613a5af5e1a0389a1990b11b07d6d251
Cameron Perrin <perrin.cameron@gmail.com>
2020-09-30 14:56:29 -0400

    beginning of new matchmaking. town room implemented.

--- Blood And Sand/Assets/Scripts/PlazaScripts/Interact.cs
+++ Blood And Sand/Assets/Scripts/PlazaScripts/Interact.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,24 +6,37 @@ public class Interact : MonoBehaviour
 {
     public int interactItem;
     public GameObject spawnLocation;
+    public GameObject networkUI;
+    public Camera playerCam;
+
+    private GameObject playerBoi;
+
 
     public void interactFunction(GameObject player){
         switch (interactItem){
-                case 1: // Mage tower
-                    Debug.Log("Mage Tower");
-                    player.transform.position = spawnLocation.transform.position;
-                    break;
-                case 2: // Blacksmith
-                    Debug.Log("Blacksmith");
-                    player.transform.position = spawnLocation.transform.position;
-                    break;
-                case 3: // Armorsmith
-                    Debug.Log("Armorsmith");
-                    player.transform.position = spawnLocation.transform.position;
-                    break;
-                default: //default
-                    Debug.Log("Default interact msg");
-                    break;
-            }
+            case 1: // Mage tower
+                Debug.Log("Mage Tower");
+                player.transform.position = spawnLocation.transform.position;
+                break;
+            case 2: // Blacksmith
+                Debug.Log("Blacksmith");
+                player.transform.position = spawnLocation.transform.position;
+                break;
+            case 3: // Armorsmith
+                Debug.Log("Armorsmith");
+                player.transform.position = spawnLocation.transform.position;
+                break;
+            case 4: // Network UI
+                Debug.Log("NetworkUI");
+                networkUI.SetActive(true);
+                playerBoi = GameObject.FindGameObjectWithTag("Player");
+                //playerBoi.transform.GetChild(0).gameObject.SetActive(false);
+                playerBoi.SetActive(false);
+                Cursor.lockState = CursorLockMode.None;
+                break;
+            default: //default
+                Debug.Log("Default interact msg");
+                break;
+        }
     }
 }
