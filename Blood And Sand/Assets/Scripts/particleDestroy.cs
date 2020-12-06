bcdc797940898744f1288a88c115b36cbbd4f9db
josh <js_itaki@yahoo.com>
2020-12-06 13:13:29 -0500

    Fixed other player's healthbar rotation

---
+++ Blood And Sand/Assets/Scripts/particleDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particleDestroy : MonoBehaviour
+{
+    
+    public float duration = 0.75f;
+
+    void Awake(){
+        Destroy(this.gameObject, duration);
+    }
+}
