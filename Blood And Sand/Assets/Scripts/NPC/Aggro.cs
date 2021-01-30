15d3b5f73708bba015b6b175bd56b0746f9393bc
trans12 <js_itaki@yahoo.com>
2021-01-29 19:08:44 -0500

    Basic NPC movement

---
+++ Blood And Sand/Assets/Scripts/NPC/Aggro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aggro : MonoBehaviour
+{
+    public GameObject NPC;
+
+    void Start()
+    {
+        NPC.GetComponent<mindlessFollow>();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            NPC.GetComponent<mindlessFollow>().followPlayer = true;
+        }
+    }
+}
---
+++ Blood And Sand/Assets/Scripts/NPC/mindlessFollow.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mindlessFollow : MonoBehaviour
+{
+
+    public bool followPlayer = false;
+    public bool stopMove = false;
+    public float moveSpeed = 0.02f;
+
+    private Vector3 playerPos;
+    private GameObject player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(followPlayer && !stopMove){
+            transform.LookAt(player.transform);
+            //Debug.Log("Player found at position: " + player.transform.position);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed);
+        }
+
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            followPlayer = true;
+            player = collision.gameObject;
+        }
+    }
+    
+    void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player"){
+            followPlayer = false;
+        }
+    }
+}
