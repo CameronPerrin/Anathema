e8ab46e5a3ec4ed6d5eeb57dfc451b46bdfcf98e
trans12 <js_itaki@yahoo.com>
2021-02-23 14:11:24 -0500

    Fixed errors push
    -Missing prefabs re-added

--- Blood And Sand/Assets/Scripts/MenuScripts/MainMenuController.cs
+++ Blood And Sand/Assets/Scripts/MenuScripts/MainMenuController.cs
@@ -3,6 +3,8 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuController : MonoBehaviourPunCallbacks
 {
@@ -16,6 +18,12 @@ public class MainMenuController : MonoBehaviourPunCallbacks
     [SerializeField]
     private GameObject loadButton;
 
+    [SerializeField]
+    //private GameObject inputField;
+
+    // input field
+    private string text;
+
     public override void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster() was called by PUN.");
@@ -26,9 +34,17 @@ public class MainMenuController : MonoBehaviourPunCallbacks
 
     public void JoinTown()
     {
-        RoomOptions roomOptions = new RoomOptions();
-        roomOptions.IsVisible = false;
-        PhotonNetwork.JoinOrCreateRoom("Anathema", roomOptions, TypedLobby.Default);
+        // grab text from input field
+        //text = inputField.GetComponent<TMP_InputField>().text;
+        // text has to be a minimum of 3 characters to work
+        //if(text.Length >= 3){
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.IsVisible = false;
+            PhotonNetwork.JoinOrCreateRoom("Anathema", roomOptions, TypedLobby.Default);
+        //}
+        //else{
+        //    Debug.Log("You entered: " + text + ". Invalid username!");
+       // }
         
     }
     public override void OnJoinedRoom()
