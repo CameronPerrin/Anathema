08100a55c29aba136d2608eeb33ddf042a0cb5e8
crypticle <cle@ltu.edu>
2021-02-23 23:08:03 -0500

    Changed gitignore file

--- Blood And Sand/Assets/Scripts/PlayerScripts/PlayerController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
-{   //used to make the equipped gameObject a child of the player prefab
-
-}
--- Blood And Sand/Assets/Stylized Grass Shader/Shaders/Libraries/StylizedGrassCommon.hlsl
+++
@@ -1,3 +0,0 @@
-/*
-I'm empty because I was renamed. Unity packages don't track deletions, so instead I'm overwritten with this content. Will be deleted in the future
-*/
